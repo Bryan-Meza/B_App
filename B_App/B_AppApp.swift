dfb7d808fb34d9e0ea46bd95f680bd0b96ff6a5d
Bryan Meza <bryanmeza1414@gmail.com>
2025-10-05 00:42:03 -0600

    Initial Commit

---
+++ B_App/B_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  B_AppApp.swift
+//  B_App
+//
+//  Created by Bryan Meza on 05/10/25.
+//
+
+import SwiftUI
+
+@main
+struct B_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
